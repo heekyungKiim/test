63b5786f02e7103dc2184029353cd82d893ac8d7
김희경 <heekyung.kim@gimhuigyeong-ui-MacBookPro.local>
2022-03-22 09:47:41 +0900

    test

---
+++ temp.js
@@ -0,0 +1,8 @@
+const { clone, set } = require('general_utils');
+const utils = require('general_utils');
+
+const date = "스트링"
+const a = [1, 1, 1, 2, 2]
+const num = "aasfd1dfs"
+// console.log(utils.sha256(num))
+console.log(utils.getSignature())
